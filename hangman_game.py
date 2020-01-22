4b62debb3daafe6111272318d85647542154c33a
rvyoges <60185328+rvyoges@users.noreply.github.com>
2020-01-22 20:41:56 +0530

    Add files via upload

---
+++ hangman_game.py
@@ -0,0 +1,33 @@
+import time
+import random 
+name = input("What is your name? ") 
+print("Hello "+name+",Let's start our Hangman game...") 
+words = ['rainbow', 'computer', 'science', 'programming',  
+         'python', 'mathematics', 'player', 'condition',  
+         'reverse', 'water', 'board', 'geeks']  
+word = random.choice(words) 
+time.sleep(2)
+print("Hint 1:Number of characters in the word:"+str(len(word)))
+print("Guess the characters") 
+guesses = ''
+turns = 10
+while (turns > 0):  
+  failed = 0
+  for char in word:  
+    if char in guesses:  
+      print(char) 
+    else:  
+      print("_") 
+      failed += 1
+  if failed == 0: 
+    print("You Win")  
+    print("The word is: ", word)  
+    break
+  guess = input("guess a character:") 
+  guesses += guess  
+  if guess not in word: 
+    turns -= 1
+    print("Wrong") 
+    print("You have", + turns, 'more guesses') 
+  if turns == 0: 
+    print("You Loose")
